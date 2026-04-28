846f41ddbf91a42a51ee0e258260dcad9e469bc5
Mikhail Pakhomov <mikhan333@mail.ru>
2026-04-28 11:45:46 +0300

    Fixes

--- init/_System/core/usr/bin/core.sh
+++ init/_System/core/usr/bin/core.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
-# echo "Test"
 sleep 30
 /etc/init.d/nginx reload
+
+# Set smaller limits for udp traffic
+sysctl -w net.netfilter.nf_conntrack_udp_timeout=5
+sysctl -w net.netfilter.nf_conntrack_udp_timeout_stream=15
